28d0d33b9c6155ff03a47ffadbddcbc2ae726668
alexandethan <67464916+AlexFMartin@users.noreply.github.com>
2022-05-04 16:20:50 +0100

    Create shuffled data

--- create_sample_data.R
+++ create_sample_data.R
@@ -1,22 +1,46 @@
 # Libraries
 library(tidyverse)
+library(haven)
+
+# Main data ---------------------------------------------------------------
 
 # Set a seed for reproducibility
 set.seed(20220504)
 
 # Load the original data (.sav file)
+df.paper3 <- read_spss("data/B2677_Barker_25Nov2021.sav")
 
 # Convert to tibble
+df.paper3 <- as_tibble(df.paper3)
 
 # Shuffle the rows
-tibble(
-  x = 1:5,
-  y = 1:5,
-  z = 1:5
-) %>% 
+df.paper3 <- df.paper3 %>%
   mutate(
     across(everything(), sample)
   ) %>% 
-  arrange(x)
+  arrange(cidB2677)
 
-# Save the data (as .sav)
+# Save the data (as .spss)
+write_sav(df.paper3, "data/shuffled data.sav")
+
+
+# Child genetic data ------------------------------------------------------
+
+# Set a seed for reproducibility
+set.seed(20220504)
+
+# Load the original data (.sav file)
+df.pgrChild <- read_csv("data/ALSPAC_PRS_Ted_corr.csv")
+
+# Convert to tibble
+df.pgrChild <- as_tibble(df.pgrChild)
+
+# Shuffle the rows
+df.pgrChild <- df.pgrChild %>%
+  mutate(
+    across(everything(), sample)
+  ) %>% 
+  arrange(cidB2677)
+
+# Save the data (as .spss)
+write_csv(df.pgrChild, "data/shuffled data pgrChild.csv")
