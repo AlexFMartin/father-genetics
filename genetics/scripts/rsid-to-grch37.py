90ffa3b2de3f3ad7a2c38a87d24a50ff3a21b86c
mjaquiery <matt.jaquiery@cs.ox.ac.uk>
2022-09-16 21:06:45 +0100

    get chr:pos indexing without lookup

--- genetics/scripts/rsid-to-grch37.py
+++ genetics/scripts/rsid-to-grch37.py
@@ -44,14 +44,22 @@ def extract_grch37(mappings: list) -> str:
 
 @click.command()
 @click.option("--file", help="Input file")
-@click.option("--rsid_column", default="rsID", help="RSID column label")
+@click.option("--chr_column", default="CHR", help="Chromosome number column")
+@click.option("--position_column", default="BP", help="Position (aka Base Pair) column")
+@click.option("--rsid_column", default="", help="RSID column label")
 @click.option("--delimiter", default="\t", help="Delimiter for input file")
 @click.option("--grch37_column", default="grch37", help="Name for the new GRCh37 column")
 @click.option("--output", default=None, help="Output file. Defaults to overwriting input file.")
-def do_conversion(file, rsid_column, delimiter, grch37_column, output):
+def do_conversion(file, rsid_column, chr_column, position_column, delimiter, grch37_column, output):
+    """
+    Add in a GRCh37 identifier to a dataset by either EBI lookup via rsID or
+    by copying chromosome:position data from other columns.
+    """
     convert(
         file=file,
         rsid_column=rsid_column,
+        chr_column=chr_column,
+        position_column=position_column,
         delimiter=delimiter,
         grch37_column=grch37_column,
         output=output
@@ -60,24 +68,31 @@ def do_conversion(file, rsid_column, delimiter, grch37_column, output):
 
 def convert(
         file: str,
-        rsid_column: [str, int] = "rsID",
+        rsid_column: [str, int] = None,
+        chr_column: str = None,
+        position_column: str = None,
         delimiter: str = "\t",
         grch37_column: str = "grch37",
         output: str = None
 ) -> None:
+    output_data = []
     with open(file, 'r') as f:
         data = [x for x in csv.DictReader(f, delimiter=delimiter)]
+
+    if not rsid_column:
+        if not chr_column or not position_column:
+            raise ValueError("BOTH chr_column AND position_column must be supplied when not using rsid_column.")
+        output_data = [{**x, grch37_column: f"{x[chr_column]}:{x[position_column]}"} for x in data]
+    else:
         rsids = [x[rsid_column] for x in data]
         print(f"Looking up {len(rsids)} unique rsIDs")
-
-    grch37s = get_grch37_by_rsids(rsids=rsids)
-    # add GRCh37 column to data
-    output_data = []
-    for x in data:
-        try:
-            output_data.append({**x, grch37_column: grch37s[x[rsid_column]]})
-        except TypeError:
-            raise RuntimeError(f"Cannot find GRCh37 entry for rsID {x[rsid_column]}")
+        grch37s = get_grch37_by_rsids(rsids=rsids)
+        # add GRCh37 column to data
+        for x in data:
+            try:
+                output_data.append({**x, grch37_column: grch37s[x[rsid_column]]})
+            except TypeError:
+                raise RuntimeError(f"Cannot find GRCh37 entry for rsID {x[rsid_column]}")
 
     if not output:
         output = file
@@ -91,5 +106,7 @@ def convert(
 if __name__ == '__main__':
     convert(
         file="../data/gwas_demo.txt",
+        chr_column="CHR",
+        position_column="BP",
         output="../data/gwas_output.txt"
     )
